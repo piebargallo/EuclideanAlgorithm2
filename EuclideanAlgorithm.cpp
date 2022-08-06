3972eac4e3ac3a430def1fd8b540b1841777e645
piebargallo <90567646+piebargallo@users.noreply.github.com>
2022-08-06 16:14:10 +0200

    Add files via upload

---
+++ EuclideanAlgorithm.cpp
@@ -0,0 +1,15 @@
+// CPP Program to demonstrate the GCD function
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Driver code
+int main() {
+	
+	// Help to function GCD
+	int r = __gcd(4, 16);
+	cout << r;
+
+	return 0;
+
+} // End driver
